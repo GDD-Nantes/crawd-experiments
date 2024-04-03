98ccead2efecb13645485de4712931e19dba531a
chat-wane <grumpy.chat.wane@gmail.com>
2024-04-03 16:45:44 +0200

    aggregators chaolee and ours

---
+++ src/main/java/fr/gdd/estimators/CountDistinctEstimator.java
@@ -0,0 +1,22 @@
+package fr.gdd.estimators;
+
+/**
+ * The interface of sample-based count-distinct estimators.
+ * @param <T> The type of the input to feed the statistics.
+ */
+public interface CountDistinctEstimator<T> {
+
+    /**
+     * Update the internal state of the estimator to take into account the new
+     * element of the sample.
+     * @param newSample The new sampled element.
+     * @return the estimator itself, for convenience.
+     */
+    CountDistinctEstimator<T> add(T newSample);
+
+    /**
+     * @return The estimate based on gathered data.
+     */
+    Double getEstimate();
+
+}
